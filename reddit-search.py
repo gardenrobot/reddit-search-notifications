bbb89ec72bf84b8da1034e6f2ab3b90d645aa63c
Samantha Clotfelter <samclot@protonmail.com>
2017-12-08 22:19:15 -0500

    Rearranged global vars

--- reddit-search.py
+++ reddit-search.py
@@ -12,26 +12,31 @@ import time
 # TODO use base dir
 # TODO user agent fix
 # TODO clean up imports
-# TODO organize constants
 # TODO make good readme
 
-url_template = 'https://www.reddit.com/r/{}/search?q={}&restrict_sr=on&include_over_18=on&sort=new&t=all'
+URL_TEMPLATE = 'https://www.reddit.com/r/{}/search?q={}&restrict_sr=on&include_over_18=on&sort=new&t=all'
 
-user_agent = 'Mozilla/5.0 (X11; Linux i586; rv:31.0) Gecko/20100101 Firefox/31.0'
+USER_AGENT = 'Mozilla/5.0 (X11; Linux i586; rv:31.0) Gecko/20100101 Firefox/31.0'
 
-meta_xpath = '//div[@class="search-result-meta"]'
+META_XPATH = '//div[@class="search-result-meta"]'
 
-interval = 3600 # 1 hour
+INTERVAL = 3600 # 1 hour
+
+POST_DATE_XPATH = './/span[@class="search-time"]/time/@datetime'
+
+POST_LINK_XPATH = './/a/@data-href-url'
+
+CONFIG_FILE = 'config'
 
 def main(subs, search, most_recent):
     subs_encode = '+'.join(subs.split(','))
     search_encode = urllib.quote_plus(search)
-    url = url_template.format(subs_encode, search_encode)
+    url = URL_TEMPLATE.format(subs_encode, search_encode)
 
     res = make_request_fake(url)
 
     res_tree = etree.HTML(res)
-    posts = res_tree.xpath(meta_xpath)
+    posts = res_tree.xpath(META_XPATH)
     if len(posts) == 0:
         print 'No posts found'
 
@@ -47,12 +52,12 @@ def main(subs, search, most_recent):
 
 # Return post date
 def get_date(post_tree):
-    time = post_tree.xpath('.//span[@class="search-time"]/time/@datetime')[0]
+    time = post_tree.xpath(POST_DATE_XPATH)[0]
     return parser.parse(time)
 
 # Return link to post
 def get_link(post_tree):
-    link = post_tree.xpath('.//a/@data-href-url')[0]
+    link = post_tree.xpath(POST_LINK_XPATH)[0]
     return link
 
 # linux specific
@@ -71,26 +76,26 @@ def make_request_fake(url):
         return f.read()
 
 def mark_read(subs, search, most_recent):
-    with open('data', 'w') as f:
+    with open(CONFIG_FILE, 'w') as f:
         f.write(subs)
         f.write(search)
         f.write(str(pytz.utc.localize(datetime.datetime.utcnow())))
 
-def read_data():
-    with open('data') as f:
+def read_config():
+    with open(CONFIG_FILE) as f:
         subs = f.readline()
         search = f.readline()
         most_recent = parser.parse(f.readline())
     return (subs, search, most_recent)
 
 if len(sys.argv) == 2 and sys.argv[1] == '-r':
-    subs, search, most_recent = read_data()
+    subs, search, most_recent = read_config()
     mark_read(subs, search, most_recent)
 elif len(sys.argv) == 1:
     while True:
-        subs, search, most_recent = read_data()
+        subs, search, most_recent = read_config()
         main(subs, search, most_recent)
-        time.sleep(interval)
+        time.sleep(INTERVAL)
 else:
     print 'Usage: python reddit-search.py [-r]'
     sys.exit
